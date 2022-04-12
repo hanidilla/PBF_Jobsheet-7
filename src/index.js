acb910c2984324684e1ca1244158d9e44767740b
Dilla Hani <hanidilla8@gmail.com>
2022-04-12 23:03:44 +0700

    React-HOC

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomFormDemo from './CustomForm/CustomFormDemo';
+import LoaderDemo from './LoadDemo/LoaderDemo';
+import GenericContainerDemo from './GenericContainer/GenericContainerDemo';
+import RequireAuthDemo from './ProtectedRoutes/RequireAuthDemo';
+import RefsDemo from './RefsDemo/RefsDemo';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CustomFormDemo/>,
   document.getElementById('root')
 );
 
